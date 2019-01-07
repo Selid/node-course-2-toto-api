09b2a98385bb3dfe8783b1d0001b0486b7c846ec
Tristan <delmas.tristan.78@gmail.com>
2019-01-07 15:05:56 +0100

    add get todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} =  require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
@@ -25,6 +26,21 @@ app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }, (err) => res.status(400).send(err));
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send("No todo with this id");
+        }
+        res.send({todo});
+    }).catch((err) => {
+        res.status(500).send("An error occured");
+    })
 });
 
 app.listen(3000, () => {
